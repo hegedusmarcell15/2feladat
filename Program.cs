fe58e3ce1d4c9289511aea6aea7a949f0298de6a
hegedusmarcell15 <hegedusm@kkszki.hu>
2024-10-11 11:54:09 +0200

    A masodik feladat elkeszult es meg lett hivva

--- Program.cs
+++ Program.cs
@@ -27,24 +27,33 @@ namespace feladat
             Console.ReadLine();
 
         }
+        public static void bank()
+        {
+            
+            Console.Write("Kérem, adja meg a lekötni kívánt összeget: ");
+            double osszeg = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Kérem, adja meg, hány évre szeretné lekötni: ");
+            int ev = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Kérem, adja meg a lekötés kamatát (százalékban): ");
+            double kamat = Convert.ToDouble(Console.ReadLine());
+
+            
+            double vegosszeg = osszeg * Math.Pow(1 + kamat / 100, ev);
+
+            Console.WriteLine($"A {ev} év múlva a lekötött összeg: {vegosszeg:F2} Ft");
+            Console.ReadKey();
+        }
+        public static void futas()
+        {
+
+        }
         static void Main(string[] args)
         {
-            DateTime ido = DateTime.Now;
-            Console.WriteLine("Jelenlegi idő: {0}:{1}.",
-                          ido.Hour, ido.Minute);
-            if (ido.Hour > 5 && ido.Hour < 9)
-            {
-                Console.WriteLine("Jó reggelt!");
-            }
-            else if (ido.Hour > 8 && ido.Hour < 19)
-            {
-                Console.WriteLine("Jó napot!");
-            }
-            else
-            {
-                Console.WriteLine("Jó éjszakat!");
-            }
-            Console.ReadLine();
+            koszon();
+            bank();
+
         }
     }
 }
